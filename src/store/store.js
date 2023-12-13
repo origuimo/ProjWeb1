2eebe00d5fa836fa292e3df1ca274913b0e2a16a
vic122112323 <v.ortizzevallos@students.salle.url.edu>
2023-12-13 15:47:56 +0100

    GuardarBearer

--- src/store/store.js
+++ src/store/store.js
@@ -4,12 +4,16 @@ import { createStore } from 'vuex';
 const store = createStore({
   state: {
     gameDataList: [], // Lista para almacenar los conjuntos de datos
+    bearer: '0',
   },
   mutations: {
     guardarJuego(state, juego) {
       juego.primerNumero = parseInt(juego.primerNumero);
       state.gameDataList.push(juego);
     },
+  },
+  guardarbearer(state, nuevoBearer) {
+    state.bearer = nuevoBearer;
   },
 });
 

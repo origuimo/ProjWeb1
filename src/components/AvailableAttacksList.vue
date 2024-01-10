239106b91caf868b352e7d9c9f44c400070abd33
vic122112323 <v.ortizzevallos@students.salle.url.edu>
2024-01-10 18:31:34 +0100

    comentando codigo

--- src/components/AvailableAttacksList.vue
+++ src/components/AvailableAttacksList.vue
@@ -1,6 +1,9 @@
 <template>
+  <!--Componente que muestra la lista filtrada de available attacks-->
   <section class="element-grid">
+    <!-- Itera sobre la lista de elementos -->
     <article v-for="element in elements" :key="element.id" @click="redirectToAttackDetail(element)">
+      <!-- Contenedor de cada elemento -->
       <div class="element-item">
         {{ element.id }}
       </div>
@@ -10,14 +13,18 @@
 
 <script>
 
-
+// Importa el componente y define las propiedades y métodos
 export default {
   props: {
+    // Propiedad que recibe la matriz de elementos
     elements: Array,
   },
   methods: {
+    // Método que se llama al hacer clic en un elemento
     redirectToAttackDetail(element) {
-      localStorage.setItem('attackItem', JSON.stringify(element))
+      // Guarda el elemento en el almacenamiento local
+      localStorage.setItem('attackItem', JSON.stringify(element));
+      // Redirige a la página de detalles del ataque utilizando Vue Router
       this.$router.push(`/attacks/${element.id}`);
     },
   },
@@ -25,17 +32,18 @@ export default {
 </script>
 
 <style scoped>
+/*Clase que define toda la pantalla*/
 .element-grid {
-  display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
-  gap: 5vw;
-  background-size: cover;
-  background-position: center;
-  background-repeat: no-repeat;
-  background-attachment: fixed;
-  max-height: 70vh;
-  overflow-y: auto;
-  margin-bottom: 1vw;
+  display: grid; /* Utiliza un diseño de cuadrícula */
+  grid-template-columns: repeat(auto-fill, minmax(150px, 1fr)); /* Columnas con ancho variable entre 150px y 1fr */
+  gap: 5vw; /* Espacio entre las celdas de la cuadrícula del 5% del ancho de la ventana */
+  background-size: cover; /* Hace que la imagen de fondo cubra completamente el contenedor */
+  background-position: center; /* Centra la imagen de fondo */
+  background-repeat: no-repeat; /* Evita la repetición de la imagen de fondo */
+  background-attachment: fixed; /* Hace que la imagen de fondo permanezca fija mientras se desplaza el contenido */
+  max-height: 70vh; /* Altura máxima del contenedor al 70% del alto de la ventana */
+  overflow-y: auto; /* Agrega una barra de desplazamiento vertical si el contenido excede la altura máxima */
+  margin-bottom: 1vw; /* Margen en la parte inferior del contenedor */
 }
  
 .element-item {
@@ -43,12 +51,12 @@ export default {
   color: white;
   padding: 1vw;
   text-align: center;
-  cursor: pointer;
+  cursor: pointer;/* Cambia el cursor a una mano al pasar sobre el elemento, indicando que es interactivo */
   font-size: 1.5vw;
   overflow: hidden;
 }
 
 .element-item:hover {
-  background-color: black;
+  background-color: pink;/* Cambia el color de fondo al pasar el ratón por encima */
 }
 </style>
